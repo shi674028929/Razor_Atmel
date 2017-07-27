9e3827b9c1a2c7c32a075bbdc34a65508634eb40
shi674028929 <674028929@qq.com>
2017-07-27 11:15:11 +0800

    Button Interface
    scanf your password

--- firmware_common/application/user_app2.c
+++ firmware_common/application/user_app2.c
@@ -38,7 +38,7 @@ extern volatile u32 G_u32ApplicationFlags;             /* From main.c */
 extern volatile u32 G_u32SystemTime1ms;                /* From board-specific source file */
 extern volatile u32 G_u32SystemTime1s;                 /* From board-specific source file */
 
-
+extern u8 G_u8DebugScanfCharCount;
 /***********************************************************************************************************************
 Global variable definitions with scope limited to this local application.
 Variable names shall start with "UserApp2_" and be declared as static.
@@ -74,6 +74,21 @@ Promises:
 */
 void UserApp2Initialize(void)
 {
+   LedOff(YELLOW);   
+   LedOff(WHITE);
+   LedOff(RED);
+   LedOff(GREEN);
+   LedOff(PURPLE);
+   PWMAudioOn(BUZZER1);
+
+
+
+
+
+   
+
+  
+ 
   /* If good initialization, set state to Idle */
   if( 1 )
   {
@@ -122,13 +137,162 @@ State Machine Function Definitions
 /* Wait for ??? */
 static void UserApp2SM_Idle(void)
 {
-    
-} /* end UserApp2SM_Idle() */
+	static u8 u8RealPassword[]={1,2,3,1,2,3};
+	static u8 u8UserPassword[]={0,0,0,0,0,0};
+	static u8 u8Index=0;
+	static u8 u8Comfirm=0;
+	static u16 u16Counter=0;
+	static u16 u16Counter2=0;
+	static bool bPressed=FALSE;
+	static bool bPressed2=FALSE;
+	static bool bIsOk=TRUE;
+	u8 u8TempIndex;
+	static u8 au8PressPassword[5];
+	static bool bISOK2=FALSE;
+	
+	if( IsButtonHeld(BUTTON3, 2000) ) 
+	{ 
+	  	LedBlink(RED, LED_2HZ); 
+		LedBlink(GREEN, LED_2HZ); 
+		bISOK2=TRUE;
+	}
+	
+	if(bISOK2)
+	{
+		if(G_u8DebugScanfCharCount==6)   //scanf your password//
+		{
+			DebugScanf(au8PressPassword);
+			u8RealPassword[0]=au8PressPassword[0]-'0';
+			u8RealPassword[1]=au8PressPassword[1]-'0';
+			u8RealPassword[2]=au8PressPassword[2]-'0';
+			u8RealPassword[3]=au8PressPassword[3]-'0';
+			u8RealPassword[4]=au8PressPassword[4]-'0';
+			u8RealPassword[5]=au8PressPassword[5]-'0';
+			DebugLineFeed();
+			G_u8DebugScanfCharCount=0;
+			DebugLineFeed();
+			LedOff(RED);
+			LedOff(GREEN);
+			bISOK2=FALSE;
+		}
+	}
+
+	if(WasButtonPressed(BUTTON3))  //confirm//
+	{
+		ButtonAcknowledge(BUTTON3);	
+		u8Index=0;
+		u8Comfirm++;
+		LedOff(WHITE);
+		LedOff(PURPLE);		
+	}
+	
+	if(u8Comfirm==2)   //compare your password//
+	{
+		for(u8TempIndex=0;u8TempIndex<6;u8TempIndex++)
+		{
+			if(u8RealPassword[u8TempIndex]!=
+			   u8UserPassword[u8TempIndex])
+			{
+				bIsOk=FALSE;
+				break;
+			}
+		}
+		
+		if(bIsOk)
+		{
+			LedOn(WHITE);
+			LedOff(PURPLE);
+		}
+		else
+		{
+			LedOff(WHITE);
+			LedOn(PURPLE);
+		}
+		LedOff(BLUE);
+		u8Comfirm=0;
+		u8Index=0;
+		u8TempIndex=0;
+		bIsOk=TRUE;
+	}
+	
+	if(u8Comfirm==1)
+	{
+		LedOn(BLUE);
+		
+		if(u8Index<6)
+		{
+			if(WasButtonPressed(BUTTON0))
+			{
+				ButtonAcknowledge(BUTTON0);
+				LedOn(RED);
+				PWMAudioOn(BUZZER1);
+				u16Counter2=0;
+				PWMAudioSetFrequency(BUZZER1,250);
+				bPressed=TRUE;
+				bPressed2=TRUE;
+				u8UserPassword[u8Index]=1;
+				u8Index++;
+			}
+		
+			if(WasButtonPressed(BUTTON1))
+			{
+				ButtonAcknowledge(BUTTON1);
+				LedOn(RED);
+				PWMAudioOn(BUZZER1);
+				u16Counter2=0;
+				PWMAudioSetFrequency(BUZZER1,250);
+				bPressed=TRUE;
+				bPressed2=TRUE;
+				u8UserPassword[u8Index]=2;
+				u8Index++;
+			}
+		
+			if(WasButtonPressed(BUTTON2))
+			{
+				ButtonAcknowledge(BUTTON2);
+				LedOn(RED);
+				PWMAudioOn(BUZZER1);
+				u16Counter2=0;
+				PWMAudioSetFrequency(BUZZER1,250);
+				bPressed=TRUE;
+				bPressed2=TRUE;
+				u8UserPassword[u8Index]=3;
+				u8Index++;
+			}
+		}
+
+			
+			if(bPressed==TRUE)
+			{
+				u16Counter++;
+				
+				if(u16Counter==100)
+				{
+					u16Counter=0;
+					LedOff(RED);
+					bPressed=FALSE;
+				}
+			}
+		
+			if(bPressed2=TRUE)
+			{
+				u16Counter2++;
+				
+				if(u16Counter2==200)
+				{
+				  	u16Counter2=0;
+					PWMAudioOff(BUZZER1);
+					bPressed2=FALSE;
+				}				
+			}
+	}
+} /* end UserAppSM_Idle() */
+
      
 #if 0
 /*-------------------------------------------------------------------------------------------------------------------*/
 /* Handle an error */
-static void UserApp2SM_Error(void)          
+static void UserApp2SM_Error(void) 
 {
   
 } /* end UserApp2SM_Error() */
